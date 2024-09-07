b1560a3075c2cf6cb167c55177e52b3ffc281ae3
Ethan Ellerstein <eellerstein@gmail.com>
2024-09-07 16:45:29 -0500

    2:37:56 - nearly done - need to do bug fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -10,7 +12,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bi-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bi-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -18,6 +20,8 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
