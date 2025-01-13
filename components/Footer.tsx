13c960d628bd58716740d700a7aa43fb7deced06
Ethan Ellerstein <eellerstein@gmail.com>
2025-01-12 19:10:33 -0600

    updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
       
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Ethan
+          Copyright © 2025 Ethan
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
